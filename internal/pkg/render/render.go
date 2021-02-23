dd47e21961f96a51d451dace48adc7ee5b18bf0d
hi20160616 <hi20160616@163.com>
2021-02-23 18:57:08 +0800

    add

--- internal/pkg/render/render.go
+++ internal/pkg/render/render.go
@@ -2,7 +2,6 @@ package render
 
 import (
 	"io"
-	"os"
 	"path"
 	"text/template"
 
@@ -12,31 +11,6 @@ import (
 type Page struct {
 	Title  string
 	Videos []*pb.Video
-}
-
-// Contents get contents from filename
-// Copy from https://golang.org/doc/effective_go#defer
-// Also this is the best practice of read file effectivily
-func Contents(filename string) (string, error) {
-	f, err := os.Open(filename)
-	if err != nil {
-		return "", err
-	}
-	defer f.Close() // f.Close will run when we're finished.
-
-	var result []byte
-	buf := make([]byte, 100)
-	for {
-		n, err := f.Read(buf[0:])
-		result = append(result, buf[0:n]...) // append is discussed later.
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			return "", err // f will be closed if we return here.
-		}
-	}
-	return string(result), nil // f will be closed if we return here.
 }
 
 // Derive derive values to be displayed.
